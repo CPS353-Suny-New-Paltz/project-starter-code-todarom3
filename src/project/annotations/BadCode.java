c1859d3d447fb99052af2a1d5bd237015a8e2abc
todarom3 <todarom3@newpaltz.edu>
2025-09-03 13:23:20 -0400

    Redo pull request

--- src/project/annotations/BadCode.java
+++ src/project/annotations/BadCode.java
@@ -3,6 +3,6 @@ package project.annotations;
 public class BadCode {
     public static void main(String[] args) {
         System.out.println("Hello World");
-
+        System.out.println("Hello Again!");
     }
 }
