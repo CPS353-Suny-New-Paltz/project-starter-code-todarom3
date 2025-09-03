2387398c184607be21301c6b997bdb7c074a0ea8
todarom3 <todarom3@newpaltz.edu>
2025-09-03 13:08:45 -0400

    Update BadCode.java
    Fixed BadCode file by removing int and adding semicolon

--- src/project/annotations/BadCode.java
+++ src/project/annotations/BadCode.java
@@ -1,7 +1,8 @@
+package project.annotations;
+
 public class BadCode {
     public static void main(String[] args) {
-        System.out.println("Hello World")
+        System.out.println("Hello World");
 
-        int unused = 42 // Missing semicolon, unused variable
     }
 }
