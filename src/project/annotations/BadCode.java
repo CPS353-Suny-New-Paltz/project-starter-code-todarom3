544772a103ef1079b0fe1d11c58db9ed90773a1c
todarom3 <todarom3@newpaltz.edu>
2025-09-03 12:50:28 -0400

    FailCode
    Code that will not work. (My files from last semester went into this project for some reason)

---
+++ src/project/annotations/BadCode.java
@@ -0,0 +1,7 @@
+public class BadCode {
+    public static void main(String[] args) {
+        System.out.println("Hello World")
+
+        int unused = 42 // Missing semicolon, unused variable
+    }
+}
