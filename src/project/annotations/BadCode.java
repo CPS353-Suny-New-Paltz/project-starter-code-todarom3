28165502e2b8f63da2d55d6c9799f5d17bdc397c
todarom3 <todarom3@newpaltz.edu>
2025-09-06 17:39:01 -0400

    Update BadCode.java
    mess up code

--- src/project/annotations/BadCode.java
+++ src/project/annotations/BadCode.java
@@ -2,6 +2,6 @@ package project.annotations;
 
 public class BadCode {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World")
     }
 }
