f163144843d54a55e385b19cf0a4f843dfd0d9a8
todarom3 <todarom3@newpaltz.edu>
2025-09-15 20:18:55 -0400

    Creating network api implementation
    The networkapi package defines how the user interacts with the compute engine. The UserRequest holds the input, UserResponse holds results, UserComputeAPI defines the actions, and UserComputeAPIImpl provides a temporary implementation.

---
+++ src/processapi/DataResponse.java
@@ -0,0 +1,15 @@
+package processapi;
+
+import java.util.List;
+
+public class DataResponse {
+    private List<Integer> data;
+
+    public DataResponse(List<Integer> data) {
+        this.data = data;
+    }
+
+    public List<Integer> getData() {
+        return data;
+    }
+}
