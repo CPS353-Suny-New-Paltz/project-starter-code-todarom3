45919867b12b8831e88aea90742bfcb976d93624
todarom3 <todarom3@newpaltz.edu>
2025-12-01 11:41:13 -0500

    Update InMemoryDataStorageAPI.java
    Fixing this file since I changed my DataStorageAPI file.

--- test/InMemoryDataStorageAPI.java
+++ test/InMemoryDataStorageAPI.java
@@ -8,6 +8,9 @@ public class InMemoryDataStorageAPI implements DataStorageAPI {
     private InMemoryInput input;
     private InMemoryOutput output;
 
+    // store delimiter even if unused
+    private String outputDelimiter = ",";
+
     public InMemoryDataStorageAPI(InMemoryInput input, InMemoryOutput output) {
         this.input = input;
         this.output = output;
@@ -22,16 +25,41 @@ public class InMemoryDataStorageAPI implements DataStorageAPI {
 
     @Override
     public DataResponse writeOutput(DataResponse response) {
-        // Write each integer as a string to the output 
-        for (Integer num : response.getData()) {
-            output.addOutput(String.valueOf(num));
+        // Write each integer as a string to the output, joined using the delimiter
+        // add each element separately to maintain test clearly
+
+        List<Integer> nums = response.getData();
+
+        if (nums == null || nums.isEmpty()) {
+            return response;
         }
 
-        // confirm success
+        // Join numbers into ONE delimited string
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.size(); i++) {
+            sb.append(nums.get(i));
+            if (i < nums.size() - 1) {
+                sb.append(outputDelimiter);
+            }
+        }
+
+        // Store that one string in output
+        output.addOutput(sb.toString());
+
         return response;
     }
 
-    // Verifying output in tests
+    @Override
+    public void setOutputDelimiter(String delimiter) {
+        // If caller passes null or empty, fall back to default comma
+        if (delimiter == null || delimiter.isEmpty()) {
+            this.outputDelimiter = ",";
+        } else {
+            this.outputDelimiter = delimiter;
+        }
+    }
+
+    // Used in tests to verify what was written
     public List<String> getWrittenOutput() {
         return output.getOutputData();
     }
